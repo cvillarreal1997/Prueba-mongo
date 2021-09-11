97e62ceed26b4e99be08024616b5e7646784df2a
Villarreal Mendieta Carlos Ramiro <43800344+cvillarreal1997@users.noreply.github.com>
2021-09-10 21:04:20 -0400

    prueba

---
+++ Pruebamongo_Villarreal/src/main/java/ec/edu/espe/distribuidas/villarreal/mongo/PruebamongoApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.distribuidas.villarreal.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebamongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebamongoApplication.class, args);
+	}
+
+}
